835ca301369edf848679c83058d861a2460905f1
Thomas Gfüllner <thomas.gfuellner@gmail.com>
2021-05-20 17:12:12 +0200

    feat: Progressbar einfacheres Argumentparing

--- start.py
+++ start.py
@@ -7,17 +7,10 @@ from matplotlib import pyplot as plt
 import pandas as pd
 import numpy as np
 
-import argparse
+import argh
+from tqdm import tqdm
 
-parser = argparse.ArgumentParser("Creates saliency and ittikochneibur maps")
-
-parser.add_argument("images", type=open, nargs="+", help="source image")
-parser.add_argument("-s", "--show", help="show maps", action="store_true")
-
-args = parser.parse_args()
-
-
-def show(img, gbvs, ittikochneibur):
+def display(img, gbvs, ittikochneibur):
     fig = plt.figure(figsize=(10, 3))
 
     fig.add_subplot(1, 3, 1)
@@ -38,30 +31,38 @@ def show(img, gbvs, ittikochneibur):
     plt.show()
 
 
+@argh.arg('images', type=open, nargs="+", help="source image(s)")
+@argh.arg("-s", "--show", help="show maps", action="store_true")
+def main(images, show=False):
+    """Creates saliency and ittikochneibur maps"""
 
-for img in args.images:
-    imgName = img.name
-    print("processing {}".format(img.name))
+    pbar = tqdm(images)
+    for img in pbar:
+        imgName = img.name
+        pbar.set_description("processing {}".format(img.name))
 
-    img.close()
-    img = cv2.imread(imgName)
+        img.close()
+        img = cv2.imread(imgName)
 
-    saliency_map_gbvs = gbvs.compute_saliency(img)
-    saliency_map_ikn = ittikochneibur.compute_saliency(img)
+        saliency_map_gbvs = gbvs.compute_saliency(img)
+        saliency_map_ikn = ittikochneibur.compute_saliency(img)
 
-    imgNameWithoutExtension = os.path.splitext(imgName)[0]
+        imgNameWithoutExtension = os.path.splitext(imgName)[0]
 
-    oname = "{}_gbvs.png".format(imgNameWithoutExtension)
-    cv2.imwrite(oname, saliency_map_gbvs)
-    norm = np.interp(saliency_map_gbvs, (saliency_map_gbvs.min(), saliency_map_gbvs.max()), (0, 1))
-    df = pd.DataFrame(data=norm)
-    df.to_csv("{}_gbvs.csv".format(imgNameWithoutExtension), index=False, header=False)
+        oname = "{}_gbvs.png".format(imgNameWithoutExtension)
+        cv2.imwrite(oname, saliency_map_gbvs)
+        norm = np.interp(saliency_map_gbvs, (saliency_map_gbvs.min(), saliency_map_gbvs.max()), (0, 1))
+        df = pd.DataFrame(data=norm)
+        df.to_csv("{}_gbvs.csv".format(imgNameWithoutExtension), index=False, header=False)
 
-    oname = "{}_ittikochneibur.png".format(imgNameWithoutExtension)
-    cv2.imwrite(oname, saliency_map_ikn)
-    norm = np.interp(saliency_map_ikn, (saliency_map_ikn.min(), saliency_map_ikn.max()), (0, 1))
-    df = pd.DataFrame(data=norm)
-    df.to_csv("{}_ittikochneibur.csv".format(imgNameWithoutExtension), index=False, header=False)
+        oname = "{}_ittikochneibur.png".format(imgNameWithoutExtension)
+        cv2.imwrite(oname, saliency_map_ikn)
+        norm = np.interp(saliency_map_ikn, (saliency_map_ikn.min(), saliency_map_ikn.max()), (0, 1))
+        df = pd.DataFrame(data=norm)
+        df.to_csv("{}_ittikochneibur.csv".format(imgNameWithoutExtension), index=False, header=False)
 
-    if args.show:
-        show(img, saliency_map_gbvs, saliency_map_ikn)
+        if show:
+            display(img, saliency_map_gbvs, saliency_map_ikn)
+
+
+argh.dispatch_command(main)
